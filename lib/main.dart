7a80dcb1d86f49bad64d1e1e1b3e2bb0b7da8538
BernardinoMarkJoshua <sm201600406@wmsu.edu.ph>
2020-10-16 13:51:55 +0800

    Hello World

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text("Hello World!"),
+      ),
+    ));
+}
+
